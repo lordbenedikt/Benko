5ef0e82a5f1f5d878e66dfc0643cdf2279648f5d
Loyyd <konradkunkel@gmx.de>
2021-01-24 23:37:46 +0100

    Archer

---
+++ Benko/Assets/Archer_test_Controller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Archer_test_Controller : MonoBehaviour
+{
+    public Animator ShootAnim;
+
+
+    private void Start()
+    {
+        //ShootAnim = GetComponent.Animator();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown("k"))
+        {
+            ShootAnim.SetBool("Shoot", true);
+        }
+
+        if (!Input.GetKeyDown("k"))
+        {
+            ShootAnim.SetBool("Shoot", false);
+        }
+    }
+}
