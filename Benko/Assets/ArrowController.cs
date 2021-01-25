382bc16d4f645fbc3d9bfa36b90f508c878506d4
Loyyd <konradkunkel@gmx.de>
2021-01-25 14:52:41 +0100

    SinusKurve

--- Benko/Assets/ArrowController.cs
+++ Benko/Assets/ArrowController.cs
@@ -6,13 +6,20 @@ public class ArrowController : MonoBehaviour
 {
     public Transform target;
     public float speed = 10.0f;
+    public Vector3 start;
+
+    
+    void Start() {
+        start = transform.position;
+    }
     public void Seek(Transform _target)
+
     {
         target = _target;
     }
     void Update()
     {
-
+        print(start);
         if(target == null)
         {
             Destroy(gameObject);
@@ -30,9 +37,17 @@ public class ArrowController : MonoBehaviour
             return;
         }
         float x = dir.normalized.x;
-        float y = Mathf.Sin(dir.normalized.y);
         float z = dir.normalized.z;
-        transform.Translate(new Vector3(x,y,z) * DistaceTime, Space.World);
+        
+        transform.Translate(new Vector3(x, 0, z) * DistaceTime, Space.World);
+
+        float travelledDistance = new Vector2(start.x-transform.position.x, start.z-transform.position.z).magnitude / new Vector2(start.x-target.position.x, start.z-target.position.z).magnitude;
+        if(travelledDistance > 1) {
+            Destroy(gameObject);
+            return;
+        }
+
+        transform.position = new Vector3(transform.position.x, Mathf.Sin((travelledDistance)*Mathf.PI)*5 + target.position.y, transform.position.z);
         
     }
 
