488fcd428a9ae2f753dd8dbb54ff1a66d5b980f8
lordbenedikt <66784958+lordbenedikt@users.noreply.github.com>
2021-01-30 17:34:58 +0100

    CustomGrid Fix

--- Benko/Assets/Scripts/CustomGrid.cs
+++ Benko/Assets/Scripts/CustomGrid.cs
@@ -243,18 +243,19 @@ public class CustomGrid : MonoBehaviour
         Vector2 v = (target-ownPos).normalized*0.2f;
         Vector2 curPos = ownPos;
         while(Vector2.Distance(curPos,target) > 0.5f) {
+            if (controller.gridIndexFromPos(curPos.x,curPos.y) == -1) break;
             if(nodes[controller.gridIndexFromPos(curPos.x,curPos.y)].GetComponent<Node>().isObstacle)
                 return false;
-            if(halfThickness!=0) {
-                if(nodes[controller.gridIndexFromPos(curPos.x+halfThickness,curPos.y)].GetComponent<Node>().isObstacle)
-                return false;
-                if(nodes[controller.gridIndexFromPos(curPos.x-halfThickness,curPos.y)].GetComponent<Node>().isObstacle)
-                return false;
-                if(nodes[controller.gridIndexFromPos(curPos.x,curPos.y+halfThickness)].GetComponent<Node>().isObstacle)
-                return false;
-                if(nodes[controller.gridIndexFromPos(curPos.x,curPos.y-halfThickness)].GetComponent<Node>().isObstacle)
-                return false;
-            }
+            // if(halfThickness!=0) {
+            //     if(nodes[controller.gridIndexFromPos(curPos.x+halfThickness,curPos.y)].GetComponent<Node>().isObstacle)
+            //     return false;
+            //     if(nodes[controller.gridIndexFromPos(curPos.x-halfThickness,curPos.y)].GetComponent<Node>().isObstacle)
+            //     return false;
+            //     if(nodes[controller.gridIndexFromPos(curPos.x,curPos.y+halfThickness)].GetComponent<Node>().isObstacle)
+            //     return false;
+            //     if(nodes[controller.gridIndexFromPos(curPos.x,curPos.y-halfThickness)].GetComponent<Node>().isObstacle)
+            //     return false;
+            // }
             curPos += v;
         }
         return true;
