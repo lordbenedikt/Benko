e2a6cfeb7b9f43c8ccfdf403f97e548eb77bd1e6
Loyyd <konradkunkel@gmx.de>
2021-02-02 19:25:12 +0100

    Eagle added

---
+++ Benko/Assets/Eagle_Controller.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eagle_Controller : MonoBehaviour
+{
+    public float speed;
+
+    void Update()
+    {
+        //transform.Translate(Vector3.forward * Time.deltaTime);
+        transform.Translate(new Vector3(-1,0,0) * Time.deltaTime);
+        transform.Rotate(new Vector3(0, 1, 0) * Time.deltaTime, 0.1f);
+        
+    }
+}
