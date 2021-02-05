50b11db2661b5cfd6288743f0669e74cd3ee0c77
Loyyd <konradkunkel@gmx.de>
2021-02-05 18:37:13 +0100

    2nd Level

---
+++ Benko/Assets/Scripts/Camera_Follower.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Follower : MonoBehaviour
+{
+    Transform PlayerTransform;
+    private Vector3 _cameraOffset;
+    [Range(0.01f, 1.0f)]
+    public float SmoothFactor = 0.5f;
+    public GameObject follower;
+
+
+	void Start()
+    {
+        PlayerTransform = follower.GetComponent<Transform>();
+        _cameraOffset = transform.position - PlayerTransform.position;
+        
+    }
+
+    void Update()
+    {
+
+
+		if (PlayerTransform != null)
+        {
+            Vector3 newPos = PlayerTransform.position + _cameraOffset;
+
+            transform.position = Vector3.Slerp(transform.position, newPos, SmoothFactor);
+        }
+    }
+}
